a064f2c384294c68395fd53998d245a2eeda5861
nlisgo <nathan@aeopus.com>
2022-02-20 13:21:11 +0000

    Support top-up of opt-out urls

--- src/Model/Subscriber.php
+++ src/Model/Subscriber.php
@@ -6,18 +6,22 @@ final class Subscriber
 {
     const PREFERENCES_URL_STEM = '/content-alerts/';
     const UNSUBSCRIBE_URL_STEM = '/content-alerts/unsubscribe/';
+    const OPTOUT_URL_STEM = '/content-alerts/optout/';
 
     private $id;
     private $preferencesUrl;
     private $unsubscribeUrl;
+    private $optoutUrl;
     private $preparePreferencesUrl = null;
     private $prepareUnsubscribeUrl = null;
+    private $prepareOptoutUrl = null;
 
-    public function __construct(int $id, string $preferencesUrl = null, string $unsubscribeUrl = null)
+    public function __construct(int $id, string $preferencesUrl = null, string $unsubscribeUrl = null, string $optoutUrl = null)
     {
         $this->id = $id;
         $this->preferencesUrl = $preferencesUrl;
         $this->unsubscribeUrl = $unsubscribeUrl;
+        $this->optoutUrl = $optoutUrl;
     }
 
     public function getId() : int
@@ -35,10 +39,16 @@ final class Subscriber
         return $this->prepareUnsubscribeUrl ?? $this->trimUrl($this->unsubscribeUrl);
     }
 
+    public function getOptoutUrl() : ?string
+    {
+        return $this->prepareOptoutUrl ?? $this->trimUrl($this->optoutUrl);
+    }
+
     public function prepareUrls() : void
     {
         $this->preparePreferencesUrl = $this->prepareUrl($this->getPreferencesUrl());
         $this->prepareUnsubscribeUrl = $this->prepareUrl($this->getUnsubscribeUrl(), self::UNSUBSCRIBE_URL_STEM);
+        $this->prepareOptoutUrl = $this->prepareUrl($this->getOptoutUrl(), self::OPTOUT_URL_STEM);
     }
 
     private function prepareUrl(string $url = null, $stem = self::PREFERENCES_URL_STEM) : string
